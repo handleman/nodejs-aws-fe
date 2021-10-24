88df278cc33dd9a26d0e47fd5d468dad8a9875f0
Pavel Hendelman <pavel.handleman@gmail.com>
2021-10-24 12:36:47 +0300

    task-8 path fix

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://1wvsccm6f0.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://3rci1wrpaf.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://handleman-cart-api-develop.eu-west-1.elasticbeanstalk.com',
+  cart: 'http://handleman-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
